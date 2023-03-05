a61a0858141b5f2fb5e491306d15e363cef2c0e5
孤灯灵 <2809551464@qq.com>
2023-03-05 18:52:57 +0800

    ✨ feat: v 1.0.0

--- README.md
+++ README.md
@@ -1,71 +1,34 @@
-# file-header README
+# file-header
 
-This is the README for your extension "file-header". After writing up a brief description, we recommend including the following sections.
+这个插件提供超级简单的文件头注释功能，支持多种语言。
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- 支持添加文件头注释: `ctrl+alt+h`
+- 支持更新文件头注释: `ctrl+alt+u`
 
-For example if there is an image subfolder under your extension project workspace:
+### 支持语言
 
-\!\[feature X\]\(images/feature-x.png\)
+有其他语言需要支持的话，可以提 issue，附上模板。
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+- JavaScript/JSX
+- TypeScript/TSX
+- Java
+- C/C++
+- C#
+- PHP
+- Rust
+- go
+- Python
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+![setting](images/2023-03-05-18-32-36.png)
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 0.0.1
 
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- 支持添加文件头注释: `ctrl+alt+h`
+- 支持更新文件头注释: `ctrl+alt+u`
+- 支持多语言
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,141 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "file-header" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-header" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const getConfig = (symbol: string) => {
+    let header = vscode.workspace.getConfiguration("file-header");
+    let author = `${symbol}@Author: ${header.get("author")}\n`;
+    let email = `${symbol}@Email: ${header.get("email")}\n`;
+    let date = `${symbol}@Date: ${new Date().toLocaleString()}\n`;
+    let lastEditors = `${symbol}@LastEditors: ${header.get("author")}\n`;
+    let lastEditTime = `${symbol}@LastEditTime: ${new Date().toLocaleString()}\n`;
+    let description = `${symbol}Description: \n`;
+    let copyright = `${symbol}Copyright: Copyright (©)}) ${new Date().getFullYear()} ${
+      header.get("copyRight") || header.get("author")
+    }. All rights reserved.\n`;
+    return {
+      author,
+      email,
+      date,
+      lastEditors,
+      lastEditTime,
+      description,
+      copyright,
+    };
+  };
+  const getSymbol = (editor: vscode.TextEditor | undefined): string[] => {
+    switch (editor?.document.languageId) {
+      case "python":
+        return ["'''", ":", "'''"];
+      case "javascript":
+      case "javascriptreact":
+      case "typescript":
+      case "typescriptreact":
+      case "java":
+      case "c":
+      case "cpp":
+      case "csharp":
+      case "go":
+      case "php":
+      case "rust":
+      case "swift":
+        return ["/**", " * ", " */"];
+      default:
+        return [];
+    }
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-header.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from file-header!');
-	});
+  let addHeader = vscode.commands.registerCommand(
+    "file-header.addFileHeader",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      let symbol = getSymbol(editor);
+      if (symbol.length === 0) {
+        vscode.window.showErrorMessage("FileHeader: 暂不支持该语言");
+        return;
+      }
+      let {
+        author,
+        email,
+        date,
+        lastEditors,
+        lastEditTime,
+        description,
+        copyright,
+      } = getConfig(symbol[1]);
 
-	context.subscriptions.push(disposable);
+      let showCopyRight = vscode.workspace
+        .getConfiguration("file-header")
+        .get("showCopyRight") as boolean;
+      let showEmail = vscode.workspace
+        .getConfiguration("file-header")
+        .get("showEmail") as boolean;
+
+      let template = `${symbol[0]}\n${author}${
+        showEmail ? email : ""
+      }${date}${lastEditors}${lastEditTime}${description}${
+        showCopyRight ? copyright : ""
+      }${symbol[2]}\n`;
+      editor?.edit((editBuilder) => {
+        // 查看是否已经有头部
+        let firstLine = editor.document.lineAt(0);
+        if (firstLine.text.startsWith(symbol[0])) {
+          vscode.window.showInformationMessage(
+            "FileHeader: 已经存在 Header 信息"
+          );
+          return;
+        }
+        // 插入头部
+        editBuilder.insert(new vscode.Position(0, 0), template);
+      });
+    }
+  );
+  let updateHeader = vscode.commands.registerCommand(
+    "file-header.updateFileHeader",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      let symbol = getSymbol(editor);
+      if (symbol.length === 0) {
+        vscode.window.showErrorMessage("暂不支持该语言");
+        return;
+      }
+      let { lastEditors, lastEditTime } = getConfig(symbol[1]);
+      editor?.edit((editBuilder) => {
+        // 查看是否已经有头部
+        let firstLine = editor.document.lineAt(0);
+        if (!firstLine.text.startsWith(symbol[0])) {
+          vscode.window.showErrorMessage("FileHeader: 还未添加 Header 信息");
+          return;
+        }
+        editBuilder.replace(
+          new vscode.Range(
+            new vscode.Position(4, 0),
+            new vscode.Position(6, 0)
+          ),
+          `${lastEditors}${lastEditTime}`
+        );
+      });
+    }
+  );
+
+  context.subscriptions.push(addHeader);
+  context.subscriptions.push(updateHeader);
 }
 
 // This method is called when your extension is deactivated
